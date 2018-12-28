b529f2b43cd5d455cd59e73e8c8e5ec72b68db24
Novrii <38958308+Novrii@users.noreply.github.com>
2018-12-28 19:37:32 +0700

    commit

--- app/src/main/java/skypea/domain/tourisapp/FragmentTiga.java
+++ app/src/main/java/skypea/domain/tourisapp/FragmentTiga.java
@@ -6,18 +6,45 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class FragmentTiga extends Fragment {
+
+    /**
+     * WebChromeClient subclass handles UI-related calls
+     * Note: think chrome as in decoration, not the Chrome browser
+     */
+    public class GeoWebChromeClient extends WebChromeClient {
+        @Override
+        public void onGeolocationPermissionsShowPrompt(String origin,
+                                                       GeolocationPermissions.Callback callback) {
+            // Always grant permission since the app itself requires location
+            // permission and the user has therefore already granted it
+            callback.invoke(origin, true, false);
+        }
+    }
+
+    WebView webView;
+
     @Nullable
     @Override
     public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =  inflater.inflate(R.layout.fragment_tiga, container, false);
 
-        WebView webView = (WebView) v.findViewById(R.id.fragment_tiga);
+        webView = (WebView) v.findViewById(R.id.fragment_tiga);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
+
+        // Brower niceties -- pinch / zoom, follow links in place
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        // Below required for geolocation
+        webView.getSettings().setGeolocationEnabled(true);
+        webView.setWebChromeClient(new GeoWebChromeClient());
+
         webView.loadUrl("http://alumni.kamaludin.ga/ai/index.php?module=hotel");
 
         return v;
